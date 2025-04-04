7fbeda6233a9759f260cad9be457f8b62286ad0e
tayyab-004 <tayyabgee333@gmail.com>
2025-04-04 15:51:25 +0500

    Fixed Bugs and Add Components

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,21 @@ import {
 const App = () => {
   return (
     <BrowserRouter>
-      <div className="relative z-0 bg-primary"></div>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+        <div className="relative z-0">
+          <Contact />
+          <StarsCanvas />
+        </div>
+      </div>
     </BrowserRouter>
   );
 };
